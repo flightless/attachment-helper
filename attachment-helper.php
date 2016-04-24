433ad81dafe978aa5f1edab79e8a9c5c3f6ef22e
Jonathan Brinley <jbrinley@flightless.us>
2016-04-24 15:23:47 -0400

    move plugin init to a static method on a class

--- attachment-helper.php
+++ attachment-helper.php
@@ -6,17 +6,4 @@
 
 require_once( __DIR__ . "/vendor/autoload.php" );
 
-/**
- * Load all the plugin files and initialize appropriately
- *
- * @return void
- */
-if ( !function_exists('attachment_helper_load') ) { // play nice
-	function attachment_helper_load() {
-		if ( defined('DOING_AJAX') && DOING_AJAX ) {
-			\AttachmentHelper\Ajax_Handler::init();
-		}
-	}
-
-	attachment_helper_load();
-}
+\AttachmentHelper\Plugin::init();
