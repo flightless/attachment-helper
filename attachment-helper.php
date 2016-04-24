0b50fba91fa9c279b19aa821799242de2370e48f
Jonathan Brinley <jbrinley@flightless.us>
2016-04-24 15:20:00 -0400

    use Composer autoloader

--- attachment-helper.php
+++ attachment-helper.php
@@ -4,24 +4,7 @@
  * A library for adding an attachment field to an admin meta box
  */
 
-
-/**
- * Auto load class files based on namespaced folders
- *
- * @return void
- */
-if( !function_exists('attachment_helper_autoload') ){
-	function attachment_helper_autoload( $class ){
-		$parts = explode('\\', $class);
-		if ( $parts[0] == 'AttachmentHelper' ) {
-			if( file_exists( dirname(__FILE__).'/'.implode(DIRECTORY_SEPARATOR, $parts).'.php' ) ){
-				require_once( dirname(__FILE__).'/'.implode(DIRECTORY_SEPARATOR, $parts).'.php' );
-			}
-		}
-	}
-}
-
-
+require_once( __DIR__ . "/vendor/autoload.php" );
 
 /**
  * Load all the plugin files and initialize appropriately
@@ -30,7 +13,6 @@ if( !function_exists('attachment_helper_autoload') ){
  */
 if ( !function_exists('attachment_helper_load') ) { // play nice
 	function attachment_helper_load() {
-		spl_autoload_register( 'attachment_helper_autoload' );
 		if ( defined('DOING_AJAX') && DOING_AJAX ) {
 			\AttachmentHelper\Ajax_Handler::init();
 		}
